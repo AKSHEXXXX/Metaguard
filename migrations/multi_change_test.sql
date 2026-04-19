8c895763e1401dd384a3deec33dc044da05d5c6b
AKSHEXXXX <AKSHEXXXX@users.noreply.github.com>
2026-04-19 16:05:36 +0530

    chore: force trigger workflow

--- migrations/multi_change_test.sql
+++ migrations/multi_change_test.sql
@@ -3,3 +3,5 @@ ALTER TABLE fact_orders DROP COLUMN created_at;
 ALTER TABLE fact_orders RENAME COLUMN customer_id TO user_id;
 ALTER TABLE fact_orders ALTER COLUMN amount TYPE DECIMAL(10,2);
 ALTER TABLE fact_orders ADD COLUMN processed_at TIMESTAMP;
+
+-- trigger
